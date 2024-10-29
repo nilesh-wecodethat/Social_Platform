a2a9c36e7e8f51c080f807403474f1b251bdaee3
nileshp-07 <patidarnilesh7223@gmail.com>
2024-10-29 16:26:21 +0530

    user-authentication

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from account.forms import LoginForm
 
-# Create your views here.
+
+def user_login(request) : 
+    if request.method == 'POST':
+       form = LoginForm(request.POST)
+
+       if form.is_valid() :
+            data = form.cleaned_data
+            user = authenticate(request,
+                               username= data['username'],
+                               password = data['password'])
+
+            if user is not None :
+                if user.is_active:
+                    login(request, user)
+
+                    return HttpResponse('Authenticated Successfully')
+                else : 
+                    return HttpResponse("Disabled Account")
+            
+            else:
+                return HttpResponse("Auth Failed")
+    
+    else:
+        form = LoginForm()
+    
+    return render(request, 'account/login.html' , {'form' : form})
