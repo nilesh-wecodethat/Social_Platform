80c626be3e2a9de4d257ac4c60402514bbac5f95
nilesh-wecodethat <nilesh@wecodethat.com>
2024-11-05 11:34:23 +0530

    Added a Image views and image ranking functionality using redis

--- Social_Platform/settings.py
+++ Social_Platform/settings.py
@@ -143,8 +143,14 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 
+
+REDIS_HOST = "localhost"
+REDIS_PORT = 6379
+REDIS_DB = 0
+
 ABSOLUTE_URL_OVERRIDES = {
     "auth.user": lambda u: reverse_lazy("user_detail", args=[u.username])
 }
 
 THUMBNAIL_DEBUG = True
+
